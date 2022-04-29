ae21f7e4b0f93fe20bc22aa21e00f06d7453243f
吴乐川 <wulechuan@live.com>
2022-04-29 16:09:00 +0800

    命令行脚本改进。

--- 用于研发阶段的命令行工具/PowerShell/Update-更新所有npm依赖包至最新版本.ps1
+++ 用于研发阶段的命令行工具/PowerShell/Update-更新所有npm依赖包至最新版本.ps1
@@ -5,12 +5,26 @@
 # ------------------------------------------------------
 
 # 以下均为须采用特定版本的【产品依赖包】。
+Write-Host
+Write-Host
+Write-Host
+Write-Host  -NoNewline  -F 'DarkRed'                '===== npm i    '
+Write-Host  -NoNewline  -F 'White'    -B 'DarkRed'  '特定版本'
+Write-Host              -F 'DarkRed'                '的【产品依赖包】 ============================'
+Write-Host
 npm i `
     'chalk@4' # 不能更新至第 5 或更晚的版本。因为自第 5 版始， chalk 仅支持 ES Module 语法。
 
 
 
 # 以下均为可采用 latest 版本的【产品依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+Write-Host
+Write-Host
+Write-Host
+Write-Host  -NoNewline  -F 'DarkRed'                '===== npm i    '
+Write-Host  -NoNewline  -F 'DarkRed'                '最末版本'
+Write-Host              -F 'DarkRed'                '的【产品依赖包】 ============================'
+Write-Host
 npm  i `
     '@wulechuan/get-valid-indentation-string@latest' `
     '@wulechuan/vue2-official-sfc-parser@latest' `
@@ -30,12 +44,26 @@ npm  i `
 # ------------------------------------------------------
 
 # 以下均为须采用特定版本的【研发依赖包】。
+Write-Host
+Write-Host
+Write-Host
+Write-Host  -NoNewline  -F 'DarkGreen'              '===== npm i -D '
+Write-Host  -NoNewline  -F 'Black'      -B 'Green'  '特定版本'
+Write-Host              -F 'DarkGreen'              '的【研发依赖包】 ============================'
+Write-Host
 npm  i  -D `
     'vue@2'
 
 
 
 # 以下均为可采用 latest 版本的【研发依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+Write-Host
+Write-Host
+Write-Host
+Write-Host  -NoNewline  -F 'DarkGreen'              '===== npm i -D '
+Write-Host  -NoNewline  -F 'DarkGreen'              '最末版本'
+Write-Host              -F 'DarkGreen'              '的【研发依赖包】 ============================'
+Write-Host
 npm  i  -D `
     '@babel/core@latest' `
     '@babel/node@latest' `
--- 用于研发阶段的命令行工具/bash/Update-更新所有npm依赖包至最新版本.sh
+++ 用于研发阶段的命令行工具/bash/Update-更新所有npm依赖包至最新版本.sh
@@ -5,12 +5,22 @@
 # ------------------------------------------------------
 
 # 以下均为须采用特定版本的【产品依赖包】。
+echo
+echo
+echo
+echo  -e  "\e[0;31m===== npm i    \e[97;41m特定版本\e[0;31m的【产品依赖包】 ============================\e[0m"
+echo
 npm i \
     'chalk@4' # 不能更新至第 5 或更晚的版本。因为自第 5 版始， chalk 仅支持 ES Module 语法。
 
 
 
 # 以下均为可采用 latest 版本的【产品依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+echo
+echo
+echo
+echo  -e  "\e[0;31m===== npm i    特定版本的【产品依赖包】 ============================\e[0m"
+echo
 npm  i \
     '@wulechuan/get-valid-indentation-string@latest' \
     '@wulechuan/vue2-official-sfc-parser@latest' \
@@ -30,12 +40,22 @@ npm  i \
 # ------------------------------------------------------
 
 # 以下均为须采用特定版本的【研发依赖包】。
+echo
+echo
+echo
+echo  -e  "\e[32m===== npm i    \e[90;102m特定版本\e[0;32m的【研发依赖包】 ============================\e[0m"
+echo
 npm  i  -D \
     'vue@2'
 
 
 
 # 以下均为可采用 latest 版本的【研发依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+echo
+echo
+echo
+echo  -e  "\e[32m===== npm i    特定版本的【研发依赖包】 ============================\e[0m"
+echo
 npm  i  -D \
     '@babel/core@latest' \
     '@babel/node@latest' \
